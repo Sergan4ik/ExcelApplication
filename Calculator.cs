ecc26008a868f8f0edb8e31653ac0719046b93bd
Sergiy Klochak <klochaksergiy@knu.ua>
2021-10-31 01:44:38 +0300

    init

---
+++ Calculator.cs
@@ -0,0 +1,48 @@
+﻿using Antlr4.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ExcelApplication
+{
+    public static class Calculator
+    {
+        public static double Evaluate(string expression , DataGrid targetGrig = null)
+        {
+            if (targetGrig != null) return 0;
+
+            ExcelApplicationLexer lexer = new ExcelApplicationLexer(new AntlrInputStream(expression));
+            lexer.RemoveErrorListeners();
+            lexer.AddErrorListener(new ThrowExceptionErrorListener());
+            var tokens = new CommonTokenStream(lexer);
+            var parser = new ExcelApplicationParser(tokens);
+            ExcelApplicationParser.CompileUnitContext tree = parser.compileUnit();
+            ExcelApplicationVisitor visitor = new ExcelApplicationVisitor(null);
+            return visitor.Visit(tree);
+        }
+
+        private static string ReplaceCellsWithValues(string _expression , DataGrid targetGrid)
+        {
+            string res = _expression;
+            string cellPattern = @"[A-Z]+[0-9]+";
+            Regex regex = new Regex(cellPattern, RegexOptions.IgnoreCase);
+            MatchCollection matches = regex.Matches(res);
+            Index number;
+            foreach (Match match in matches)
+            {
+                if (targetGrid.Dictionary.ContainsKey(match.Value))
+                {
+                    number = NumberConverter.ConvertFrom26System(match.Value);
+                }
+            }
+            MatchEvaluator mEvaluator = new MatchEvaluator(targetGrid.RefToValue);
+            string newExpr = regex.Replace(res, mEvaluator);
+
+
+            return res;
+        }
+    }
+}
---
+++ ExcelApplication.g4
@@ -0,0 +1,42 @@
+﻿grammar ExcelApplication;
+
+/*
+* Parser Rules
+*/
+
+compileUnit : expression EOF;
+expression :
+	LPAREN expression RPAREN #ParenthesizedExpr
+	|expression EXPONENT expression #ExponentialExpr
+	|expression operatorToken=(MOD | DIV) expression #ModDivExpr
+	|expression operatorToken=(MULTIPLY | DIVIDE) expression #MultiplicativeExpr
+	|expression operatorToken=(ADD | SUBTRACT) expression #AdditiveExpr
+	|operatorToken=(MMIN | MMAX) LPAREN expression (DESP expression)* RPAREN #MminMmaxExpr
+	|operatorToken=(INC| DEC) LPAREN expression RPAREN #IncDecExpr
+	|NUMBER #NumberExpr
+	|IDENTIFIER #IdentifierExpr
+	;
+
+/*
+* Lexer Rules
+*/
+
+NUMBER : INT (',' INT)?;
+IDENTIFIER : [a-zA-Z]+[1-9][0-9]+;
+INT : ('0'..'9')+;
+
+INC: 'inc';
+DEC: 'dec';
+EXPONENT : '^';
+MULTIPLY : '*';
+DIVIDE : '/';
+SUBTRACT : '-';
+ADD : '+';
+MMIN : 'MMIN';
+MMAX : 'MMAX';
+MOD : 'MOD';
+DIV : 'DIV';
+DESP : ',';
+LPAREN : '(';
+RPAREN : ')';
+WS : [ \t\r\n] -> channel(HIDDEN);
